ff8e474900125f4c4a3623f7a3e40c133c62bae5
kyle1992 <joycek2@mymail.nku.edu>
2015-03-16 19:12:04 -0400

    Added Project Files
    HTML, Javascript, and Phonegap files added

---
+++ project_folder/Simplify/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Kyle\Documents\School\Spring 2015\Digital Media Projects\Digital Media Projects App\app\Simplify\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\Kyle\Documents\School\Spring 2015\Digital Media Projects\Digital Media Projects App\app\Simplify\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ project_folder/Simplify/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\Kyle\Documents\School\Spring 2015\Digital Media Projects\Digital Media Projects App\app\Simplify\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\Users\Kyle\Documents\School\Spring 2015\Digital Media Projects\Digital Media Projects App\app\Simplify\platforms\android\ant-build\CordovaApp.ap_ \
+C:\Users\Kyle\Documents\School\Spring 2015\Digital Media Projects\Digital Media Projects App\app\Simplify\platforms\android\ant-build\classes.dex \
+C:\Users\Kyle\Documents\School\Spring 2015\Digital Media Projects\Digital Media Projects App\app\Simplify\platforms\android\CordovaLib\ant-build\classes.jar \
